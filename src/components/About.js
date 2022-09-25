e313afd88bcb73371feb5e254b4f652379e59a0d
Joanne Imbisi <evizengwa@yahoo.com>
2022-09-25 10:12:28 -0700

    About me paragraph

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,18 @@
-function App(){
+function About(){
 
-return <div>
+return (
+  <div>
     <h2> Joanne I </h2>
 
-    <h4> Hello everyone! My name is Joanne Imbisi and I'm a 26 year old software engineering student. I decided to choose this path because I enjoy solving problems and working remotely. One major benefit of working remotely is being able to travel/live in different parts of the world without having to worry about your job physically needing you back </h4>
-</div>;
+    <h4>
+      Hello everyone! My name is Joanne Imbisi and I'm a 26 year old software
+      engineering student. I decided to choose this path because I enjoy
+      problem solving and working remotely. One major benefit of working remotely is
+      being able to travel/live in different parts of the world without having
+      to worry about your job physically requiring you to be there.
+    </h4>
+  </div>
+);
 }
 
-export default App
+export default About
