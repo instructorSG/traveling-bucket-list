744cf598b2addbe196fa98b2f4dea26fb1dd2974
Joanne Imbisi <evizengwa@yahoo.com>
2022-09-25 10:15:06 -0700

    Add Visited component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,36 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+// import "./Navbar.css"
 
 function NavBar() {
-    return (
-        <nav>
-            <NavLink exact to="/">Home</NavLink>
+  return (
+    <nav className="nav">
+      <div>
+        <h4> Travel Bucket List </h4>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>
+            <NavLink exact to="/">
+              Home
+            </NavLink>
+          </li>
+          <li>
             <NavLink to="/about">About</NavLink>
+          </li>
+          <li>
             <NavLink to="/details">Details</NavLink>
-        </nav>
-    );
-} 
+          </li>
+          <li>
+            <NavLink to="bucketlist">Bucket List</NavLink>
+          </li>
+          <li>
+            <NavLink to="visited">Visited</NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
 
 export default NavBar;
