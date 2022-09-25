bd35ec20ac135ec6d173caece3901d2e8eb706f7
Joanne Imbisi <evizengwa@yahoo.com>
2022-09-25 10:10:33 -0700

    Get request for cities

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,28 @@
-import React, { useState } from "react";
-import { Route, Switch } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./Home";
 import About from "./About"; 
+import Details from "./Details"; 
 import NavBar from "./NavBar";
+import NotFound from "./NotFound";
+import BucketList from "./BucketList";
+import Visited from "./Visited"
+
 
 function App() {
     const [page, setPage] = useState("/")
+    const [cities, setCities] = useState([])
+
+   useEffect(() => {
+       fetch('http://localhost:3000/cities')
+       .then(response => response.json())
+       .then(cities => setCities(cities)) 
+   }, [])
+
+   
+   
   return (
-    <div>
+    <BrowserRouter>
       <NavBar onChangePage={setPage} />
       <Switch>
         <Route exact path="/">
@@ -19,8 +34,17 @@ function App() {
         <Route path="/details">
           <Details />
         </Route>
+        <Route path="/bucketlist">
+          <BucketList cities={cities}/>
+        </Route>
+        <Route path="/visited">
+          <Visited />
+        </Route>
+        <Route path="*">
+          <NotFound/>
+        </Route>
       </Switch>
-    </div>
+    </BrowserRouter >
   );
 }
 
